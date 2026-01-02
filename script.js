df6c42c1579def90245e81b854c88707ef5e6771
David <davidviaxespam@gmail.com>
2026-01-01 18:47:47 -0800

    Checkbox & movement

--- script.js
+++ script.js
@@ -9,5 +9,28 @@ button.addEventListener("click", () => {
     li.textContent = newHabit;
     document.getElementById("habit-list").appendChild(li);
     input.value = "";
+
+    // create checkbox img
+    const checkboxImg = document.createElement("img");
+    checkboxImg.src = "images/unchecked.png";
+    checkboxImg.width = 20;
+    checkboxImg.height = 20;
+
+    checkboxImg.addEventListener("click", () => {
+      if (checkboxImg.src.includes("unchecked")) {
+        checkboxImg.src = "images/checked.png";
+
+        // move to bottom
+        document.getElementById("habit-list").appendChild(li);
+
+      } else {
+        checkboxImg.src = "images/unchecked.png";
+
+        // move to top
+        document.getElementById("habit-list").insertBefore(li, document.getElementById("habit-list").firstChild);
+      }
+    });
+
+    li.appendChild(checkboxImg);
   }
 });
